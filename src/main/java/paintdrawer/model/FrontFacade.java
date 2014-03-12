b53fc2ebf6bd555597c3aef1fe6732f0aece80be
Joel Denke <mail@happyness.se>
2014-03-12 12:39:36 +0100

    Merged the facades into one

--- src/main/java/paintdrawer/model/FrontFacade.java
+++ src/main/java/paintdrawer/model/FrontFacade.java
@@ -1,12 +1,44 @@
 package paintdrawer.model;
 
+import java.awt.*;
 import java.io.File;
+import java.util.*;
 
 /**
- * Created by joel on 2014-03-12.
+ * @author Mats Maatson, Joel Denke
+ *
+ * A class that implements the faced pattern hiding complexity of the model
+ *
  */
-public class FrontFacade
+public class FrontFacade extends Observable
 {
+    java.util.List<paintdrawer.model.abstracts.Shape> drawnShapes;
+    java.util.List<paintdrawer.model.abstracts.Shape> prototypeShapes;
+
+    public FrontFacade()
+    {
+        drawnShapes = new ArrayList<paintdrawer.model.abstracts.Shape>();
+        prototypeShapes = new ArrayList<paintdrawer.model.abstracts.Shape>();
+    }
+
+    public void drawShape(String shapeName, int x, int y, int lineWidth, boolean filled, Color color)
+    {
+        paintdrawer.model.abstracts.Shape shape = null;
+
+        for (paintdrawer.model.abstracts.Shape s : prototypeShapes) {
+            if (s.getName().equalsIgnoreCase(shapeName)) {
+                shape = s.cloneShape();
+                break;
+            }
+        }
+
+        if (shape != null) {
+            shape.init(color, lineWidth, filled, x, y);
+            drawnShapes.add(shape);
+            setChanged();
+            notifyObservers();
+        }
+    }
 
     public boolean save(File file)
     {
